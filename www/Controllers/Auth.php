e1e2b0709cc50c25e0a04d0ad7b5fa1d498f0add
Quentin Bonhoure <bonhoure.quentin@gmail.com>
2025-12-10 17:30:53 +0100

    [EDIT] Modification de la regex pour la création de compte le mot de passe demande désormais également un caratère spécial

--- www/Controllers/Auth.php
+++ www/Controllers/Auth.php
@@ -90,9 +90,10 @@ class Auth
                 strlen($_POST["pwd"]) < 8 ||
                 !preg_match('#[A-Z]#', $_POST["pwd"]) ||
                 !preg_match('#[a-z]#', $_POST["pwd"]) ||
-                !preg_match('#[0-9]#', $_POST["pwd"])
-            ) {
-                $errors[] = "Le mot de passe doit faire au moins 8 caractères avec une minuscule, une majuscule et un chiffres";
+                !preg_match('#[0-9]#', $_POST["pwd"]) ||
+                !preg_match('#[^a-zA-Z0-9_-]#', $_POST["pwd"])
+                ){
+                $errors[] = "Le mot de passe doit faire au moins 8 caractères avec une minuscule, une majuscule, un chiffre et un caractère spécial.";
             }
 
             if ($_POST["pwd"] != $_POST["pwdConfirm"]) {
